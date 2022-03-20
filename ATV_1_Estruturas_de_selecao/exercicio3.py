3729a0c42d38d8945c5614059250cdc0fc9c7ac5
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-03-20 19:08:25 -0300

    Update exercicio3.py

--- ATV_1_Estruturas_de_selecao/exercicio3.py
+++ ATV_1_Estruturas_de_selecao/exercicio3.py
@@ -1,12 +1,11 @@
 print("CALCULO DE PESO IDEAL \n")
-sexo = str(input("Digite o sexo (M/F): ")).upper()
-altura = float(input("Digite sua altura: "))
 
-if sexo == "F":
-    peso_ideal = (62.1 * altura) - 44.7
-    print("Seu peso ideal é: {} Kg".format(round(peso_ideal, 2))) 
-elif sexo == "M":
-    peso_ideal = (72.7 * altura) - 58
-    print("Seu peso ideal é: {} Kg".format(round(peso_ideal, 2))) 
-else:
-    print("Parametro invalido")
+def pesoIdeal(sexo, altura):
+    if sexo == "F" or sexo == "f":
+        peso_ideal = (62.1 * altura) - 44.7
+        print("Seu peso ideal é: {} Kg".format(round(peso_ideal, 2))) 
+    elif sexo == "M" or sexo == "m":
+        peso_ideal = (72.7 * altura) - 58
+        print("Seu peso ideal é: {} Kg".format(round(peso_ideal, 2))) 
+    else:
+        print("Parametro invalido")
