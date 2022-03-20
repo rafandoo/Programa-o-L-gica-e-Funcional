ffe90f734f0bed4dd1e23533ea13b17cd08f2002
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-03-20 19:08:06 -0300

    Update exercicio2.py

--- ATV_1_Estruturas_de_selecao/exercicio2.py
+++ ATV_1_Estruturas_de_selecao/exercicio2.py
@@ -6,18 +6,17 @@ categorias = [
     "SENIOR"
 ]
 
-idade = int(input("Digite a idade do competidor: "))
-
-if idade >= 5 and idade <=7:
-    print("A categoria do competidor é: {}".format(categorias[0]))
-elif idade >= 8 and idade <= 10:
-    print("A categoria do competidor é: {}".format(categorias[1]))
-elif idade >= 11 and idade <= 13:
-    print("A categoria do competidor é: {}".format(categorias[2]))
-elif idade >= 14 and idade <= 17:
-    print("A categoria do competidor é: {}".format(categorias[3]))
-elif idade >= 18:
-    print("A categoria do competidor é: {}".format(categorias[4]))
-else:
-    print("Categoria não listada")
+def verificaCategoria(idade):
+    if idade >= 5 and idade <=7:
+        return("A categoria do competidor é: {}".format(categorias[0]))
+    elif idade >= 8 and idade <= 10:
+        return("A categoria do competidor é: {}".format(categorias[1]))
+    elif idade >= 11 and idade <= 13:
+        return("A categoria do competidor é: {}".format(categorias[2]))
+    elif idade >= 14 and idade <= 17:
+        return("A categoria do competidor é: {}".format(categorias[3]))
+    elif idade >= 18:
+        return("A categoria do competidor é: {}".format(categorias[4]))
+    else:
+        return("Categoria não listada")
     
