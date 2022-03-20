3bf4e6586aa7660a3751ffbd41a410f08e4a56c9
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-03-20 19:08:31 -0300

    Update exercicio4.py

--- ATV_1_Estruturas_de_selecao/exercicio4.py
+++ ATV_1_Estruturas_de_selecao/exercicio4.py
@@ -12,33 +12,10 @@ meses = [
     "Novembro",
     "Dezembro"
 ]
-
-mes = int(input("Digite um numero de um mes (EX: 2): "))
-
-if mes == 1:
-    print("O mes {} é {}".format(1, meses[mes-1]))
-elif mes == 2:
-    print("O mes {} é {}".format(2, meses[mes-1]))
-elif mes == 3:
-    print("O mes {} é {}".format(3, meses[mes-1]))
-elif mes == 4:
-    print("O mes {} é {}".format(4, meses[mes-1]))
-elif mes == 5:
-    print("O mes {} é {}".format(5, meses[mes-1]))
-elif mes == 6:
-    print("O mes {} é {}".format(6, meses[mes-1]))
-elif mes == 7:
-    print("O mes {} é {}".format(7, meses[mes-1]))
-elif mes == 8:
-    print("O mes {} é {}".format(8, meses[mes-1]))
-elif mes == 9:
-    print("O mes {} é {}".format(9, meses[mes-1]))
-elif mes == 10:
-    print("O mes {} é {}".format(10, meses[mes-1]))
-elif mes == 11:
-    print("O mes {} é {}".format(11, meses[mes-1]))
-elif mes == 12:
-    print("O mes {} é {}".format(12, meses[mes-1]))
-else: 
-    print("Numero informado não corresponde a nenhum mes!")
+    
+def retornaMesNumero(mes): 
+    if mes >= 1 and mes <=12:
+        return("O mes {} é {}".format(mes, meses[mes-1]))
+    else:
+        return("Numero informado não corresponde a nenhum mes!")
     
