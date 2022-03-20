34db073f0b98c4a6b48a3a5b2adfc66d4f8ae9da
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-03-20 19:08:01 -0300

    Update exercicio1.py

--- ATV_1_Estruturas_de_selecao/exercicio1.py
+++ ATV_1_Estruturas_de_selecao/exercicio1.py
@@ -1,5 +1,5 @@
-valor = int(input("Digite uma valor: "))
-if valor > 10 and valor < 80:
-    print("O valor esta entre 10 e 80")
-else:
-    print("o valor não esta entre 10 e 80")
+def compara(valor):
+    if valor > 10 and valor < 80:
+        return "O valor esta entre 10 e 80"
+    else:
+        return "O valor nao esta entre 10 e 80"
