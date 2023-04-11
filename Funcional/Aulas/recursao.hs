ef7b4ffd0a696e51fd0d558480b313e4423bcabc
rafandoo <rafaelcamargo.inf@gmail.com>
2023-04-11 11:20:53 -0300

    Update aula 11-04

--- Funcional/Aulas/recursao.hs
+++ Funcional/Aulas/recursao.hs
@@ -16,9 +16,22 @@ fatorial n = n * fatorial (n-1)
 maior [x] = x
 maior (x:xs) = max x (maior xs)
 
+dobroElementos [] = []
+dobroElementos (x:xs) = (x*2) : dobroElementos xs
+
+
+ultimoElemento :: [Int] -> Maybe Int
+ultimoElemento [] = Nothing
+ultimoElemento (x:xs) = if xs == [] then Just x else ultimoElemento xs
+
+
 main = do
-    print (somatorio 3)
-    print (outraSoma 3)
-    print (somaLista [1,2,3,4,5])
-    print (outraSomaLista [1,2,3,4,5])
-    print (fatorial 5)
+    -- print (somatorio 3)
+    -- print (outraSoma 3)
+    -- print (somaLista [1,2,3,4,5])
+    -- print (outraSomaLista [1,2,3,4,5])
+    -- print (fatorial 5)
+    -- print (maior [1,2,3,4,5])
+    -- print (dobroElementos [1,2,3,4,5])
+    print (ultimoElemento [])
+    print (ultimoElemento [1, 2, 3, 4, 5])
